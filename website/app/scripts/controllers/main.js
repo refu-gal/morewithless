9d64f956b61ba29344c728c8569a68292c941ddc
David Constenla <daconstenla@gmail.com>
2017-03-05 12:15:52 +0100

    Added login/logout behaviour

--- website/app/scripts/controllers/main.js
+++ website/app/scripts/controllers/main.js
@@ -8,16 +8,71 @@
  * Controller of the morewithlessApp
  */
 angular.module('morewithlessApp')
-  .controller('MainCtrl', function ($scope) {
-    
-    $scope.title1 = 'Button';
-    $scope.title4 = 'Warn';
-    $scope.isDisabled = true;
-    $scope.googleUrl = 'http://google.com';
+.controller('MainCtrl', function ($location,$scope, $mdDialog, $rootScope,campaign,user,authentification) {
+  
+  $scope.campaigns = []
+  $scope.campaigns = campaign.getAllCampaigns()
+  $scope.users = []
+  $scope.users = user.getAllusers()
+  
+  authentification.verifyCredentials()
+  $scope.showLoginDialog = function(ev){
+    $mdDialog.show({
+      controller: "LoginDialogController",
+      templateUrl: 'views/logindialog.html',
+      parent: angular.element(document.body),
+      targetEvent: ev,
+      clickOutsideToClose:true,
+      fullscreen: $scope.customFullscreen // Only for -xs, -sm breakpoints.
+    })
+    .then(function(answer) {
+      $scope.status = 'You said the information was "' + answer + '".'
+      console.log($scope.status)
+    }, function() {
+      $scope.status = 'You cancelled the dialog.'
+      console.log($scope.status)
+    });
+  }
+  $scope.logout = function(){
+    authentification.clearCredentials()
+  }
 
-    this.awesomeThings = [
-      'HTML5 Boilerplate',
-      'AngularJS',
-      'Karma'
-    ];
-  });
+  $scope.goToProfile = function(){
+    user.getByUsername($rootScope.globals.currentUser.username).then(
+      function(data){
+        console.log("data %O",data)
+        if(data.isCompany){
+          console.log('im a company, jump to company view')
+          $location.path( "/company" );
+        }
+        if(!data.isAuthorized) return
+        console.log('im not a company, jump to worker view')
+        $location.path( "/worker" );
+      },
+      function(err){
+        console.error("error is %O",err)
+      }
+    )
+  }
+
+})
+.controller('LoginDialogController', function ($scope, $mdDialog, authentification) {
+  $scope.cancel = function(){
+    $mdDialog.hide();
+  }
+  $scope.login = function(username,password){
+    console.log("trying to login")
+    $scope.loginusername = username
+    $scope.loginpassword = password
+    authentification.login(username,password, loginResult)
+  }
+  function loginResult(data){
+    if(data.success){
+      authentification.setCredentials($scope.loginusername,$scope.loginpassword)
+      $mdDialog.hide("login");
+    }else{
+      authentification.clearCredentials()
+      $scope.status = data.message
+    }
+  }
+})
--- website/app/scripts/services/user.js
+++ website/app/scripts/services/user.js
@@ -8,6 +8,23 @@
  * Service in the morewithlessApp.
  */
 angular.module('morewithlessApp')
-  .service('user', function () {
-    // AngularJS will instantiate a singleton by calling "new" on this function
+  .service('user', function ($resource) {
+    
+    var userResource = $resource(
+      location.protocol + '//' + location.host+':8080'+'/users/:username',
+      {username: '@username'}
+    )
+      
+    function getAllusers(){
+      return userResource.query()
+    }
+    function getByUsername(username){
+      return userResource.get({username:username}).$promise
+    }
+
+    var userFactory = {
+      getAllusers: getAllusers,
+      getByUsername: getByUsername
+    }
+    return userFactory
   });
